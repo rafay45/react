e4b4568d0963ad64f691b5beb9a28187f6839946
rafay45 <rafayy579@gmail.com>
2025-04-23 21:56:58 +0500

    🎡🧨Practicing Raact

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Random from "./Script"
+function App() {
+  return (
+    <>
+ <h1>Hello World !!</h1>
+ <Random />
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Script.jsx
@@ -0,0 +1,7 @@
+function Random (){
+    return(
+        <h2>I Created Random Component | @rafay</h2>
+    )
+}
+
+export default Random;
