444f2acbeb90b6ca1d1552089ff187dc6bb0b12a
rafay45 <rafayy579@gmail.com>
2025-05-06 13:06:19 +0500

    🎡🧨Practicing React

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -4,32 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("DarkCyan");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full h-screen duration-200 mt-0' style={{ backgroundColor: color }}>
+        <div id='change' className='flex flex-wrap gap-3 rounded-3xl inset-x-40 justify-around p-2 fixed bottom-10 px-2' style={{backgroundColor: color === "white" ? "black" : "white"}}>
+          <button className='w-25 shadow-2xl cursor-pointer rounded-full text-white font-bold font-sans border-none p-2' style={{ backgroundColor: "green" }} onClick={() => setColor("green")}>Green</button>
+          <button className='w-25 shadow-2xl cursor-pointer rounded-full text-white font-bold font-sans border-none p-2' style={{ backgroundColor: "blue" }} onClick={() => setColor("blue")}>Blue</button>
+          <button className='w-25 shadow-2xl cursor-pointer rounded-full text-white font-bold font-sans border-none p-2' style={{ backgroundColor: "red" }} onClick={() => setColor("red")}>Red</button>
+          <button className='w-25 shadow-2xl cursor-pointer rounded-full text-white font-bold font-sans border-none p-2' style={{ backgroundColor: "Deeppink" }} onClick={() => setColor("pink")}>Pink</button>
+          <button className='w-25 shadow-2xl cursor-pointer rounded-full text-white font-bold font-sans border-none p-2' style={{ backgroundColor: "Gold" }} onClick={() => setColor("yellow")}>Yellow</button>
+          <button className='w-25 shadow-2xl cursor-pointer rounded-full text-black font-bold font-sans border-none p-2 bg-blue-100' onClick={() => setColor("white")}>White</button>
+          <button id='changeBlack' className='w-25 cursor-pointer rounded-full font-bold font-sans border-none p-2' style={{backgroundColor: color === "white" ? "white" : "black", color: color === "white" ? "black" : "white"}} onClick={() => setColor("black")}>Black</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
