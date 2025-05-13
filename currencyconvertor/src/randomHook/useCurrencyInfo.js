d6b66cab53f6daea1cee51852b63beeed889d78b
rafay45 <rafayy579@gmail.com>
2025-05-13 23:21:47 +0500

    🎡🧨Practicing React

--- currencyconvertor/src/randomHook/useCurrencyInfo.js
+++ currencyconvertor/src/randomHook/useCurrencyInfo.js
@@ -1,9 +1,13 @@
 import { useState, useEffect} from "react";
 
 function useCurrencyInfo(currency){
+  const [data, setData] = useState({})
     useEffect(() => {
       let data =  fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+      .then(response => response.json()).then(response => setData(response[currency]))
       console.log(data);
-      
-    }, [])
+    }, [currency])
+       return data;
 }
+
+export default useCurrencyInfo;
