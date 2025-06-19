3684d1b4967829ba06cc195de0c1164b82adcea2
rafay45 <rafayy579@gmail.com>
2025-06-19 20:01:39 +0500

    🎡🧨Practicing React

---
+++ todowithreduxtoolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: [{
+        id: 1,
+        text: "Learn Redux Toolkit",
+    }]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodos: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+           state.todos = state.todos.filter((todo) => todo.id.payload !== action.payload)
+        }
+    }
+})
+
+export const {addTodos, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
