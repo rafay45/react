f44103d798c3521f7f03d80ecc19d3b8436a99ca
rafay45 <rafayy579@gmail.com>
2025-06-27 00:37:08 +0500

    🎡🧨Practicing React

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -34,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
