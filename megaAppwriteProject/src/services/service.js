ed5b5031188943925aa89d34b3cdeaf96cee628b
rafay45 <rafayy579@gmail.com>
2025-07-08 14:13:45 +0500

    🎡🧨Practicing React

--- megaAppwriteProject/src/services/service.js
+++ megaAppwriteProject/src/services/service.js
@@ -6,8 +6,63 @@ export class Services {
     databases;
     storage;
 
-    constructor(){
-     
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.storage = new Storage(this.client)
+    }
+    async createPost({ title, slug, content, featuredImage, userId, status }) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log('AppwriteDatabaseServices :: Error is createDocument ::', error);
+
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+          console.log('AppwriteDatabaseServices :: Error is updateDocument ::', error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+             await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+             console.log('AppwriteDatabaseServices :: Error is deleteDocument ::', error);
+             return false;
+        }
     }
 }
 
