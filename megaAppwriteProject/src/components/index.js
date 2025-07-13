cfd12372105f358c29464eebe12c832b633587e4
rafay45 <rafayy579@gmail.com>
2025-07-13 16:56:16 +0500

    🎡🧨Practicing React

--- megaAppwriteProject/src/components/index.js
+++ megaAppwriteProject/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import Button from "./Button";
 
 export {
     Header, 
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button
 }
