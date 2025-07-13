efa5dd6684d0ec61519c14d65b90838713c234cd
rafay45 <rafayy579@gmail.com>
2025-07-13 18:54:28 +0500

    🎡🧨Practicing React

---
+++ megaAppwriteProject/src/components/Protect.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import authStatus from '../store/authSlice'
+import { useSelector } from 'react-redux'
+
+function Protect({ childern, authentication = true }) {
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+    const authStatus = useSelector((state) => state.authentication.status)
+    useEffect(() => {
+        if (authentication && authStatus !== authentication) {
+            navigate('/login')
+        } else if (!authentication && authStatus !== authentication) {
+            navigate('/')   
+        }
+        setLoading(false)
+    }, [authStatus, authentication, navigate])
+    return (
+        <div>
+        {loading ? <h1>Loading...</h1> : <>{childern}</>}
+        </div>
+    )
+}
+
+export default Protect
