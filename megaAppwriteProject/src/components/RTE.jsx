69e243cb62b110f1549aae978c5039c50a6c6c7a
rafay45 <rafayy579@gmail.com>
2025-07-15 10:44:28 +0500

    🎡🧨Practicing React

--- megaAppwriteProject/src/components/RTE.jsx
+++ megaAppwriteProject/src/components/RTE.jsx
@@ -1,20 +1,51 @@
 import { Controller } from 'react-hook-form'
 import { Editor } from '@tinymce/tinymce-react'
 
-export default function RTE({name, control, label, defaultValue = ""}) {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div>
-        {label && <label className=''></label>}
-         <Controller
-         name= {name || 'content'}
-         control={control}
-         rules={{required: true}}
-         render={({field: {onChange}}) => (
-                <Editor 
-                initialValue={defaultValue}
-                />
-         ) }
+      {label && <label className=''></label>}
+      <Controller
+        name={name || 'content'}
+        control={control}
+        rules={{ required: true }}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "code",
+                "help",
+                "wordcount",
+                "anchor",
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+            }}
+            onEditorChange={onChange}
           />
+        )}
+      />
     </div>
   )
 }
