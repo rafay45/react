523b2c81379a81e6f77b89658b7e8d69dffcadf5
rafay45 <rafayy579@gmail.com>
2025-07-21 07:03:37 +0500

    🎡🧨Practicing React

--- todoWithContextApi/src/App.jsx
+++ todoWithContextApi/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
     <TodoContextProvider value={{ todos, addTodo, deleteTodo, editTodo, toggleTodo }}>
       <div className="bg-gray-100 min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md bg-gray-200 rounded-lg px-4 py-3 text-black">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your ToDos</h1>
           <div className="mb-4">
             <AddTodos />
           </div>
